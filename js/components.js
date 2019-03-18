6a4ad849574334832d7d4fc4f7304da83a99bb63
FeofanYang <yyf0326@qq.com>
2019-03-18 18:14:09 +0800

    使用 prettier 格式化

--- js/components.js
+++ js/components.js
@@ -1,133 +1,133 @@
 // 图片组件
 Vue.component('base-img', {
-	props: {
-		image: Object,
-		origin: { type: Boolean, default: false },
-		center: { type: Boolean, default: false },
-		middle: { type: Boolean, default: false }
-	},
-	computed: {
-		setStyle: function () {
-			let style = {};
-			if (!this.origin) {
-				style.width = this.image.width / 100 + 'rem';
-			}
-			if (this.center) {
-				style.position = 'absolute';
-				style.left = '50%';
-				style.width = this.image.width / 100 + 'rem';
-				style.marginLeft = -this.image.width / 200 + 'rem';
-			}
-			if (this.middle) {
-				style.position = 'absolute';
-				style.top = '50%';
-				style.height = this.image.height / 100 + 'rem';
-				style.marginTop = -this.image.height / 200 + 'rem';
-			}
-			return style;
-		}
-	},
-	template: `<img v-if="image" :src="image.src" :style="setStyle"></img>`
+  props: {
+    image: Object,
+    origin: { type: Boolean, default: false },
+    center: { type: Boolean, default: false },
+    middle: { type: Boolean, default: false }
+  },
+  computed: {
+    setStyle: function() {
+      let style = {};
+      if (!this.origin) {
+        style.width = this.image.width / 100 + 'rem';
+      }
+      if (this.center) {
+        style.position = 'absolute';
+        style.left = '50%';
+        style.width = this.image.width / 100 + 'rem';
+        style.marginLeft = -this.image.width / 200 + 'rem';
+      }
+      if (this.middle) {
+        style.position = 'absolute';
+        style.top = '50%';
+        style.height = this.image.height / 100 + 'rem';
+        style.marginTop = -this.image.height / 200 + 'rem';
+      }
+      return style;
+    }
+  },
+  template: `<img v-if="image" :src="image.src" :style="setStyle"></img>`
 });
 // 弹出层组件
 Vue.component('popup', {
-	props: {
-		visible: { type: Boolean, default: true },
-		closeOnClickOverlay: { type: Boolean, default: true },
-		width: { type: [String, Number], default: '4rem' },
-		position: {
-			default: 5,
-			validator: function (value) {
-				return [1, 2, 3, 4, 5, 6, 7, 8, 9].indexOf(value) !== -1;
-			}
-		},
-		offset: { type: [String, Number], default: 0 },
-		image: { type: [Object, String] },
-		text: { type: [String, Number] },
-		color: { type: String, default: '#fff' },
-		background: { type: String, default: 'rgba(0,0,0,.7)' },
-	},
-	data() {
-		return {
-			textStyle: {
-				marginTop: '.1rem',
-				textAlign: 'center',
-				color: this.color
-			}
-		}
-	},
-	computed: {
-		wrapStyle: function () {
-			let style = {
-				position: 'absolute',
-				width: this.width
-			}
-			if (typeof this.image === 'object') {
-				style.width = this.image.width / 100 + 'rem';
-			}
-			let offset = this.offset;
-			switch (this.position) {
-				case 1:
-					style.top = offset;
-					style.left = offset;
-					break;
-				case 2:
-					style.top = offset;
-					style.left = '50%';
-					style.transform = 'translate(-50%,0)';
-					break;
-				case 3:
-					style.top = offset;
-					style.right = offset;
-					break;
-				case 4:
-					style.top = '50%';
-					style.left = offset;
-					style.transform = 'translate(0,-50%)';
-					break;
-				case 5:
-					style.top = '50%';
-					style.left = '50%';
-					style.transform = 'translate(-50%,-50%)';
-					break;
-				case 6:
-					style.top = '50%';
-					style.right = offset;
-					style.transform = 'translate(0,-50%)';
-					break;
-				case 7:
-					style.bottom = offset;
-					style.left = offset;
-					break;
-				case 8:
-					style.bottom = offset;
-					style.left = '50%';
-					style.transform = 'translate(-50%,0)';
-					break;
-				case 9:
-					style.right = offset;
-					style.bottom = offset;
-					break;
-			}
-			return style;
-		},
-		ImageSrc: function () {
-			if (typeof this.image === 'object') {
-				return this.image.src;
-			}
-			if (typeof this.image === 'string') {
-				return this.image;
-			}
-		},
-	},
-	methods: {
-		clickOverlay() {
-			if (this.closeOnClickOverlay === true) {
-				this.$emit('update:visible', false);
-			}
-		}
-	},
-	template: `
+  props: {
+    visible: { type: Boolean, default: true },
+    closeOnClickOverlay: { type: Boolean, default: true },
+    width: { type: [String, Number], default: '4rem' },
+    position: {
+      default: 5,
+      validator: function(value) {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9].indexOf(value) !== -1;
+      }
+    },
+    offset: { type: [String, Number], default: 0 },
+    image: { type: [Object, String] },
+    text: { type: [String, Number] },
+    color: { type: String, default: '#fff' },
+    background: { type: String, default: 'rgba(0,0,0,.7)' }
+  },
+  data() {
+    return {
+      textStyle: {
+        marginTop: '.1rem',
+        textAlign: 'center',
+        color: this.color
+      }
+    };
+  },
+  computed: {
+    wrapStyle: function() {
+      let style = {
+        position: 'absolute',
+        width: this.width
+      };
+      if (typeof this.image === 'object') {
+        style.width = this.image.width / 100 + 'rem';
+      }
+      let offset = this.offset;
+      switch (this.position) {
+        case 1:
+          style.top = offset;
+          style.left = offset;
+          break;
+        case 2:
+          style.top = offset;
+          style.left = '50%';
+          style.transform = 'translate(-50%,0)';
+          break;
+        case 3:
+          style.top = offset;
+          style.right = offset;
+          break;
+        case 4:
+          style.top = '50%';
+          style.left = offset;
+          style.transform = 'translate(0,-50%)';
+          break;
+        case 5:
+          style.top = '50%';
+          style.left = '50%';
+          style.transform = 'translate(-50%,-50%)';
+          break;
+        case 6:
+          style.top = '50%';
+          style.right = offset;
+          style.transform = 'translate(0,-50%)';
+          break;
+        case 7:
+          style.bottom = offset;
+          style.left = offset;
+          break;
+        case 8:
+          style.bottom = offset;
+          style.left = '50%';
+          style.transform = 'translate(-50%,0)';
+          break;
+        case 9:
+          style.right = offset;
+          style.bottom = offset;
+          break;
+      }
+      return style;
+    },
+    ImageSrc: function() {
+      if (typeof this.image === 'object') {
+        return this.image.src;
+      }
+      if (typeof this.image === 'string') {
+        return this.image;
+      }
+    }
+  },
+  methods: {
+    clickOverlay() {
+      if (this.closeOnClickOverlay === true) {
+        this.$emit('update:visible', false);
+      }
+    }
+  },
+  template: `
 		<transition name="fade">
 			<div v-if="visible" @click="clickOverlay" :style="{backgroundColor:background}" class="page--popup">
 				<div :style="wrapStyle">
--- js/index.js
+++ js/index.js
@@ -1,84 +1,98 @@
 // 设置全局 rem
-window.onresize = function () {
-	document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + "px";
-}
+window.onresize = function() {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+};
 onresize();
 
 // 禁用 touchmove
-document.body.addEventListener('touchmove', function (e) {
-	e.preventDefault();
-}, { passive: false });
+document.body.addEventListener(
+  'touchmove',
+  function(e) {
+    e.preventDefault();
+  },
+  { passive: false }
+);
 
 // 自动播放音乐
-(function () {
-	let audio = document.getElementById('music');
-	audio.play();
-	document.addEventListener('WeixinJSBridgeReady', function () {
-		audio.play();
-	}, false);
-	document.addEventListener('YixinJSBridgeReady', function () {
-		audio.play();
-	}, false);
+(function() {
+  let audio = document.getElementById('music');
+  audio.play();
+  document.addEventListener(
+    'WeixinJSBridgeReady',
+    function() {
+      audio.play();
+    },
+    false
+  );
+  document.addEventListener(
+    'YixinJSBridgeReady',
+    function() {
+      audio.play();
+    },
+    false
+  );
 })();
 
 // Vue
 var h5 = new Vue({
-	el: '#h5',
-	data: {
-		oImg: {},
-		bLoadV: true,
-		nLoadNum: 0,
-		bIndexV: false,
-		oMusicStatus: 'i-music-on',
-	},
-	mounted() {
-		let that = this;
-		let queue = new createjs.LoadQueue();
-		queue.on('progress', function (e) {
-			that.nLoadNum = parseInt(e.progress * 100);
-		});
-		queue.on('fileload', function (target) {
-			if (target.item.type === 'image') {
-				that.oImg[target.item.id] = {
-					src: target.item.src,
-					width: target.result.width,
-					height: target.result.height
-				};
-			}
-		});
-		queue.on('complete', function () {
-			// 显示首页
-			that.bLoadV = false;
-			that.bIndexV = true;
-		});
-		let manifest = [
-			// 所有需要预加载的资源
-			{ 'id': 'BGM', 'src': 'media/music.mp3' },
-		];
-		queue.loadManifest(manifest);
-	},
-	methods: {
-		toggleMusic: function () {
-			let audio = document.getElementById('music');
-			if (audio.paused) {
-				audio.play();
-				this.oMusicStatus = 'i-music-on';
-			} else {
-				audio.pause();
-				this.oMusicStatus = 'i-music-off';
-			}
-		},
-		inputRepair: function () {
-			let timer, pos, distance = 1;
-			timer = setInterval(function () {
-				pos = document.documentElement.scrollTop || document.body.scrollTop;
-				pos -= distance;
-				window.scrollTo(0, pos);
-				pos += distance;
-				window.scrollTo(0, pos);
-				clearInterval(timer);
-			}, 1);
-			console.log('repair done');
-		},
-	},
+  el: '#h5',
+  data: {
+    oImg: {},
+    bLoadV: true,
+    nLoadNum: 0,
+    bIndexV: false,
+    oMusicStatus: 'i-music-on'
+  },
+  mounted() {
+    let that = this;
+    let queue = new createjs.LoadQueue();
+    queue.on('progress', function(e) {
+      that.nLoadNum = parseInt(e.progress * 100);
+    });
+    queue.on('fileload', function(target) {
+      if (target.item.type === 'image') {
+        that.oImg[target.item.id] = {
+          src: target.item.src,
+          width: target.result.width,
+          height: target.result.height
+        };
+      }
+    });
+    queue.on('complete', function() {
+      // 显示首页
+      that.bLoadV = false;
+      that.bIndexV = true;
+    });
+    let manifest = [
+      // 所有需要预加载的资源
+      { id: 'BGM', src: 'media/music.mp3' }
+    ];
+    queue.loadManifest(manifest);
+  },
+  methods: {
+    toggleMusic: function() {
+      let audio = document.getElementById('music');
+      if (audio.paused) {
+        audio.play();
+        this.oMusicStatus = 'i-music-on';
+      } else {
+        audio.pause();
+        this.oMusicStatus = 'i-music-off';
+      }
+    },
+    inputRepair: function() {
+      let timer,
+        pos,
+        distance = 1;
+      timer = setInterval(function() {
+        pos = document.documentElement.scrollTop || document.body.scrollTop;
+        pos -= distance;
+        window.scrollTo(0, pos);
+        pos += distance;
+        window.scrollTo(0, pos);
+        clearInterval(timer);
+      }, 1);
+      console.log('repair done');
+    }
+  }
 });
